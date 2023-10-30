00aa9fb65df0c7ae5b1548d06994600570257e9e
Keshara1997 <kavindakeshara97@gmail.com>
2023-10-30 10:46:09 +0530

    The le keyword

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-window.onload = function() {
-    const pi = 3.142;
+// window.onload = function() {
+//     const pi = 3.142;
 
-    function calculateArea(radius){
-        return pi * radius * radius;
-    }
+//     function calculateArea(radius){
+//         return pi * radius * radius;
+//     }
    
 
-    let radius = prompt("Enter the radius of the circle");
-    let area = calculateArea(radius);
-    console.log("Area of the circle is: " + area);
+//     let radius = prompt("Enter the radius of the circle");
+//     let area = calculateArea(radius);
+//     console.log("Area of the circle is: " + area);
       
 
-}
+// }
 
+
+window.onload = function() {
+
+    var items = document.getElementsByTagName("li");
+    
+    for (var i = 0; i < items.length; i++) {
+        items[i].onclick = function(e) {
+            console.log(e.target.innerHTML);
+        }
+    } 
+}
